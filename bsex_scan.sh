21b850dfb4c975afe8d7f3f7833988c611257154
h4r7w3l1 <36492399+h4r7w3l1@users.noreply.github.com>
2023-03-25 21:20:01 +0300

    Fix issues

--- bsex_scan.sh
+++ bsex_scan.sh
@@ -5,27 +5,32 @@ chatid=''
 SaveDate=$(date +%Y.%m.%d)
 
 # Get and save mullvad socks5 proxy in-mem array
-Socks5Arr+=("$(curl -s https://api.mullvad.net/www/relays/wireguard/ | jq -r '.[] .socks_name')")
+Socks5Arr+=($(curl -s https://api.mullvad.net/www/relays/wireguard/ | jq -r '.[] .socks_name'))
 Socks5ArrSize=$(printf "%s\n" "${Socks5Arr[@]}"  | wc -l)
+
 # Function for select random value from array
 selectRndProxy() {
-    index=$(("$RANDOM" % "$Socks5ArrSize"))
+    Socks5ArrSize=$(printf "%s\n" "${Socks5Arr[@]}"  | wc -l)
+    index=$(($RANDOM % $Socks5ArrSize))
     echo "${Socks5Arr[$index]}"
 }
 
+
+
 # Get and save all active exchangers ID (check `unzip -hh` for read about `funzip`). Sort and uniq result.
-ExchIDsArr+=("$(curl -sL api.bestchange.ru/info.zip | funzip - |  cut -d ';' -f1 | sort | uniq )")
+ExchIDsArr+=($(curl -sL api.bestchange.ru/info.zip | funzip - | awk -F';' '{print $1}' | sort | uniq ))
 ExchIDsArrSize=$(printf "%s\n" "${ExchIDsArr[@]}"  | wc -l)
 
 # Inform about working datasets
 printf "Uniq IDs: %s\nCount Socks5: %s\n\n"  "${ExchIDsArrSize}"  "${Socks5ArrSize}"
 
-for line in "${ExchIDsArr[@]}";
+for name in "${ExchIDsArr[@]}"
  do
-    name="$line"
     # Loop the request until we get a response from the server
     while true; do
-        response=$(curl -sI "https://www.bestchange.ru/click.php?id=$name" -w '%{redirect_url}\n' -o /dev/null --socks5-hostname "$(selectRndProxy)")
+        ProxyHost=$(selectRndProxy)
+        #echo "DEBUG curl -sI "https://www.bestchange.ru/click.php?id=$name" -w '%{redirect_url}\n' -o /dev/null --socks5-hostname "${ProxyHost}""
+        response=$(curl -sI "https://www.bestchange.ru/click.php?id=$name" -w '%{redirect_url}\n' -o /dev/null --socks5-hostname "${ProxyHost}")
         if [ $? -eq 0 ]; then
             if [[ -n "$response" ]]; then
                 echo "$response"
