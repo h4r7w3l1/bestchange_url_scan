36097ada0fa530ca57b85434032b1dd6e7c87b74
lifescore <lifescore@auth.cfd>
2023-03-25 19:57:21 +0300

    Features to avoid using file on FS

--- bsex_scan.sh
+++ bsex_scan.sh
@@ -2,21 +2,30 @@
 
 BOTsecret=''
 chatid=''
+SaveDate=$(date +%Y.%m.%d)
 
-# Get and save mullvad socks5 proxy
-curl -s https://api.mullvad.net/www/relays/wireguard/ | jq -r '.[] .socks_name' -> socks5.txt
+# Get and save mullvad socks5 proxy in-mem array
+Socks5Arr+=("$(curl -s https://api.mullvad.net/www/relays/wireguard/ | jq -r '.[] .socks_name')")
+Socks5ArrSize=$(printf "%s\n" "${Socks5Arr[@]}"  | wc -l)
+# Function for select random value from array
+selectRndProxy() {
+    index=$(("$RANDOM" % "$Socks5ArrSize"))
+    echo "${Socks5Arr[$index]}"
+}
 
-# Get and save all active exchangers ID
-wget http://api.bestchange.ru/info.zip -O info.zip && unzip -o -j info.zip bm_exch.dat && iconv -f windows-1251 -t UTF-8 bm_exch.dat > bm_exchnew.dat && cut -d ';' -f1 bm_exchnew.dat > id.txt
+# Get and save all active exchangers ID (check `unzip -hh` for read about `funzip`). Sort and uniq result.
+ExchIDsArr+=("$(curl -sL api.bestchange.ru/info.zip | funzip - |  cut -d ';' -f1 | sort | uniq )")
+ExchIDsArrSize=$(printf "%s\n" "${ExchIDsArr[@]}"  | wc -l)
 
-# Read the id.txt file line by line and get all the URLs of the exchangers
-while read -r line; do
+# Inform about working datasets
+printf "Uniq IDs: %s\nCount Socks5: %s\n\n"  "${ExchIDsArrSize}"  "${Socks5ArrSize}"
+
+for line in "${ExchIDsArr[@]}";
+ do
     name="$line"
     # Loop the request until we get a response from the server
     while true; do
-        # Using curl to get server response with Mullvad socks5 proxy for each exchange ID
-        response=$(curl -sI "https://www.bestchange.ru/click.php?id=$name" -w '%{redirect_url}\n' -o /dev/null --socks5-hostname "$(sort -R socks5.txt | head -n1)")
-        # Check the status of the response and display the response if it is received
+        response=$(curl -sI "https://www.bestchange.ru/click.php?id=$name" -w '%{redirect_url}\n' -o /dev/null --socks5-hostname "$(selectRndProxy)")
         if [ $? -eq 0 ]; then
             if [[ -n "$response" ]]; then
                 echo "$response"
@@ -30,7 +39,7 @@ while read -r line; do
             sleep 2
         fi
     done
-done < id.txt > urls.txt
+done >> "./urls_$SaveDate.txt"
 
 # Send result to tg_channel
-curl -F document=@"./urls.txt" -F caption="BestChange_$(date +'%Y-%m-%d')" "https://api.telegram.org/bot$BOTsecret/sendDocument?chat_id=$chatid" --noproxy '*'
+curl -F document=@"./urls_$SaveDate.txt" -F caption="BestChange_$(date +'%Y-%m-%d')" "https://api.telegram.org/bot$BOTsecret/sendDocument?chat_id=$chatid" --noproxy '*'
